aa541a522e9e3196d1af6dc25e436add5f519275
Danny Ochoa <danny@dannyochoa.net>
2025-10-21 23:00:02 -0400

    Update nvim setup

---
+++ nvim/lua/danny/init.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader = " " -- Set leader key before Lazy
+
+require("danny.lazy_init")
+require("danny.remap")
+require("danny.set")
---
+++ nvim/lua/danny/lazy/comment.lua
@@ -0,0 +1,17 @@
+return {
+  'numToStr/Comment.nvim',
+  config = function()
+    require('Comment').setup()
+
+    -- TODO: See if i can change this to CMD + / somehow
+    -- Ctrl+_ for commenting
+    local api = require('Comment.api')
+    vim.keymap.set('n', '<C-_>', api.toggle.linewise.current)
+    vim.keymap.set('i', '<C-_>', api.toggle.linewise.current)
+    vim.keymap.set('v', '<C-_>', function()
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'nx', false)
+      api.toggle.linewise(vim.fn.visualmode())
+    end)
+  end
+}
+
