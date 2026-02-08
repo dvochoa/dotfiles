62980bb7bfbfaa50afb9c3967a8a06ae4cb2a538
Danny Ochoa <danny@dannyochoa.net>
2026-02-08 16:07:46 -0500

    Center on use of telescope livegrep

--- nvim/lua/danny/plugins/telescope.lua
+++ nvim/lua/danny/plugins/telescope.lua
@@ -3,6 +3,27 @@ return {
   tag = '0.1.6', -- or, branch = '0.1.x',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    require('telescope').setup({})
+    local actions = require('telescope.actions')
+
+    require('telescope').setup({
+      defaults = {
+        mappings = {
+          i = {
+            -- Center view after selecting in insert mode
+            ["<CR>"] = function(prompt_bufnr)
+              actions.select_default(prompt_bufnr)
+              vim.cmd('normal! zz')
+            end,
+          },
+          n = {
+            -- Center view after selecting in normal mode
+            ["<CR>"] = function(prompt_bufnr)
+              actions.select_default(prompt_bufnr)
+              vim.cmd('normal! zz')
+            end,
+          },
+        },
+      },
+    })
   end
 }
