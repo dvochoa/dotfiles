e47fa9f7da237b8b6cba3846c1e472e1442b722a
Danny Ochoa <danny@dannyochoa.net>
2026-05-07 22:03:27 -0400

    Fixing random shit

--- nvim/lua/danny/plugins/colorscheme.lua
+++ nvim/lua/danny/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   "folke/tokyonight.nvim",
   config = function()
     require("tokyonight").setup({
-      style = "storm",                  -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+      style = "night",                  -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
       transparent = true,               -- Enable this to disable setting the background color
       terminal_colors = true,           -- Configure the colors used when opening a `:terminal` in Neovim
       styles = {
@@ -11,7 +11,7 @@ return {
         comments = { italic = false },
         keywords = { italic = false },
         -- Background styles. Can be "dark", "transparent" or "normal"
-        sidebars = "dark",             -- style for sidebars, see below
+        sidebars = "transparent",      -- style for sidebars, see below
         floats = "dark",               -- style for floating windows
       },
     })
