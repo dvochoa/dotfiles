4b2d9533e6a6df7e3a0c761e52f0f085693eefc4
Danny Ochoa <danny@dannyochoa.net>
2026-09-21 23:03:27 -0400

    fixup! Read AGENTS.md natively and remap comment toggle to Ctrl+/

--- nvim/lua/danny/plugins/comment.lua
+++ nvim/lua/danny/plugins/comment.lua
@@ -6,13 +6,16 @@ return {
       pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
     })
 
-    -- Ctrl+/ for commenting
+    -- Ctrl+/ & Ctrl+_ for commenting
+    -- Prefer Ctrl+/ but include Ctrl+_ for environments without Kitty keyboard protocol support
     local api = require('Comment.api')
-    vim.keymap.set('n', '<C-/>', api.toggle.linewise.current)
-    vim.keymap.set('i', '<C-/>', api.toggle.linewise.current)
-    vim.keymap.set('v', '<C-/>', function()
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'nx', false)
-      api.toggle.linewise(vim.fn.visualmode())
-    end)
+    for _, key in ipairs({ '<C-/>', '<C-_>' }) do
+      vim.keymap.set('n', key, api.toggle.linewise.current)
+      vim.keymap.set('i', key, api.toggle.linewise.current)
+      vim.keymap.set('v', key, function()
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'nx', false)
+        api.toggle.linewise(vim.fn.visualmode())
+      end)
+    end
   end
 }
